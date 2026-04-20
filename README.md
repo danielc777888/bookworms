02b6e00bf4f7988740d7f13a927bb6ed8a3dd044
daniel-dev <danielc777888@gmail.com>
2026-04-20 22:35:08 +0200

    Add formatting and README file

---
+++ README.md
@@ -0,0 +1,9 @@
+# bookworms
+Book library web app with Haskell.
+
+## install
+- Install ghcup: https://www.haskell.org/ghcup/
+- Execute: _./setghc_
+
+## run server
+- Execute: _cabal run_
--- src/Bookworms/Server.hs
+++ src/Bookworms/Server.hs
@@ -10,9 +10,6 @@
 
 module Bookworms.Server where
 
-import Prelude ()
-import Prelude.Compat
-
 import Control.Monad.Except
 import Control.Monad.Reader
 import Data.Aeson
@@ -28,28 +25,31 @@ import Lucid
 import Network.HTTP.Media ((//), (/:))
 import Network.Wai
 import Network.Wai.Handler.Warp
+import Prelude.Compat
 import Servant
+import Servant.Types.SourceT (source)
 import System.Directory
 import Text.Blaze
-import Text.Blaze.Html.Renderer.Utf8
-import Servant.Types.SourceT (source)
 import qualified Text.Blaze.Html
+import Text.Blaze.Html.Renderer.Utf8
+import Prelude ()
 
 type UserAPI1 = "users" :> Get '[JSON] [User]
 
-data User = User {
-    name :: String,
-    age :: Int,
-    email :: String,
-    registration_date :: Day
-  } deriving (Eq, Show, Generic)
+data User = User
+  { name :: String
+  , age :: Int
+  , email :: String
+  , registration_date :: Day
+  }
+  deriving (Eq, Show, Generic)
 
 instance ToJSON User
 
 users1 :: [User]
-users1 = [
-    User "Isaac Newton" 372 "isaac@newton.co.uk" (fromGregorian 1683 3 1),
-    User "Albert Einstein" 136 "ae@mc2.org"      (fromGregorian 1905 21 1)
+users1 =
+  [ User "Isaac Newton" 372 "isaac@newton.co.uk" (fromGregorian 1683 3 1)
+  , User "Albert Einstein" 136 "ae@mc2.org" (fromGregorian 1905 21 1)
   ]
 
 server1 :: Server UserAPI1
@@ -63,4 +63,3 @@ app1 = serve userAPI server1
 
 runServer :: IO ()
 runServer = run 8081 app1
-
